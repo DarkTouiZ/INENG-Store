bec7249196e5c9dba0938bbcaa4983640cb581e0
HOOd-00 <105187293+HOOd-00@users.noreply.github.com>
2023-12-17 11:55:10 +0700

    Move Cart to src\page

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,7 +1,12 @@
-import Nav from "@/components/Navbar";
-import Center from "@/components/Center";
+import Nav from "../../components/Navbar";
+import Center from "../../components/Center";
 import styled from "styled-components";
-import Button from "@/components/Button";
+import Button from "../../components/Button";
+
+// import Nav from "@/components/Navbar";
+// import Center from "@/components/Center";
+// import styled from "styled-components";
+// import Button from "@/components/Button";
 
 const ColumnsWrapper = styled.div`
     display: grid;
